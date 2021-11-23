1e24df9280f9f2d3380a80db3aa25f3c538e9bd8
ignacio taylor <iggytaylor@hotmail.com>
2021-11-23 10:58:29 +0100

    fast.py changes

--- api/fast.py
+++ api/fast.py
@@ -7,6 +7,10 @@ import joblib
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import make_blobs
+
+
 app = FastAPI()
 
 app.add_middleware(
@@ -21,5 +25,16 @@ app.add_middleware(
 #TEST of api localhost
 
 @app.get("/predict")
-def index():
-    return {"ok": True}
+def index(num1,num2):
+    # generate 2d classification dataset
+    X, y = make_blobs(n_samples=100, centers=2, n_features=2, random_state=1)
+    # fit final model
+    model = LogisticRegression()
+    model.fit(X, y)
+
+    # define one new instance
+    Xnew = [[num1, num2]]
+    # make a prediction
+    ynew = model.predict(Xnew)
+
+    return ("X=%s, Predicted=%s" % (Xnew[0], ynew[0]))
