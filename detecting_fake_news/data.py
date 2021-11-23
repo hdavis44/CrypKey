3353410dfa91fd552b6dd8fd7ef219811e1fff02
Henry Davis <henryo.davis@gmail.com>
2021-11-23 12:02:58 +0100

    working on data.py

---
+++ detecting_fake_news/data.py
@@ -0,0 +1,29 @@
+from google.cloud import storage
+import pandas as pd
+
+
+
+BUCKET_NAME='wagon-data-745-fake-news-data'
+
+BUCKET_TRAIN_DATA_PATH='data/train.csv'
+
+LOCAL_TRAIN_DATA_PATH='''
+    /home/henry/code/hdavis44/detecting_fake_news/raw_data/train.csv
+'''
+
+
+def get_cloud_data(nrows=None):
+    '''method to get training data from google cloud bucket'''
+    df = pd.read_csv(f"gs://{BUCKET_NAME}/{BUCKET_TRAIN_DATA_PATH}",
+                     nrows=nrows)
+    return df
+
+def get_local_data(nrows=None):
+    '''method to get training data from local machine'''
+    df = pd.read_csv(LOCAL_TRAIN_DATA_PATH, nrows=nrows)
+    return df
+
+
+
+if __name__=='__main__':
+    df = get_cloud_data()
