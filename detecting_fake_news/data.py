98699c0b5e23cedabadf3461501392bc7c1ffd95
Henry Davis <henryo.davis@gmail.com>
2021-11-24 11:14:56 +0100

    created working trainer.py which outputs a model.joblib file containing a pipeline

--- detecting_fake_news/data.py
+++ detecting_fake_news/data.py
@@ -6,12 +6,20 @@ from detecting_fake_news.params import BUCKET_NAME, BUCKET_TRAIN_DATA_PATH, LOCA
 
 def get_cloud_data(nrows=None):
     '''method to get training data from google cloud bucket'''
+    if nrows:
+        print(f"getting {nrows} rows of cloud data")
+    else:
+        print("getting cloud data")
     df = pd.read_csv(f"gs://{BUCKET_NAME}/{BUCKET_TRAIN_DATA_PATH}",
                      nrows=nrows)
     return df
 
 def get_local_data(nrows=None):
     '''method to get training data from local machine'''
+    if nrows:
+        print(f"getting {nrows} rows of local data")
+    else:
+        print("getting local data")
     df = pd.read_csv(LOCAL_TRAIN_DATA_PATH, nrows=nrows)
     return df
 
