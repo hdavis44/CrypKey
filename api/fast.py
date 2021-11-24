5ccd1db9cea73248c9638ac295058e91a6ee730e
ignacio taylor <iggytaylor@hotmail.com>
2021-11-24 17:31:35 +0100

    changes

--- api/fast.py
+++ api/fast.py
@@ -7,6 +7,9 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from detecting_fake_news.preprocessing import TextPreprocessor
+#from detecting_fake_news.predict import read_bucket_model
+from detecting_fake_news.params import  PATH_TO_LOCAL_MODEL
+
 
 
 app = FastAPI()
@@ -24,10 +27,7 @@ app.add_middleware(
 )
 
 
-
-
-
-
+#api prediction with local model
 @app.get("/predict")
 def predictt(text):
 
@@ -48,3 +48,28 @@ def predictt(text):
 
     #api output
     return {'prediction' :results}
+
+
+
+
+#api prediction with model in the cloud
+# @app.get("/predict_cloud")
+# def predictt(text):
+
+#     # text form strign to df
+#     text = pd.DataFrame(dict(text=[text]))
+
+#     #clean text, remove punctuation, etc
+#     clean_text = TextPreprocessor().transform(text)
+
+#     #load the model.joblib that is already train
+#     model = read_bucket_model(PATH_TO_LOCAL_MODEL)
+
+#     # predict the model with the new text
+#     prediction_local = model.predict(clean_text)
+
+#     # change the output type
+#     results = int(prediction_local[0])
+
+#     #api output
+#     return {'prediction': results}
