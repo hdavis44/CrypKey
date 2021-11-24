da42743150d2eafc1e924869a0b2d80364fe46ca
ignacio taylor <iggytaylor@hotmail.com>
2021-11-24 11:14:53 +0100

    fast and predict .py

--- detecting_fake_news/predict.py
+++ detecting_fake_news/predict.py
@@ -3,23 +3,43 @@ import pandas as pd
 import os
 from detecting_fake_news.preprocessing import *
 
-
+from google.cloud import storage
+from tempfile import TemporaryFile
 
 
 
 # run the predict from the local model.joblib
 def predict_local(text):
 
-    clean_text = TextPreprocessor().clean_text(text)
 
-    model = joblib.load('DETECTING_FAKE_NEWS/model.joblib')
+    text = pd.DataFrame(dict(text=[text]))
+    print('working 1', text)
 
-    prediction_local= model.predict(clean_text)
+    clean_text = TextPreprocessor().transform(text)
+    print('working 2', clean_text)
+
+    model = joblib.load('../model.joblib')
+    print('working 3', model)
+
+    prediction_local = model.predict(clean_text)
+    print('working 4', prediction_local)
 
     results = int(prediction_local[0])
+    print('working 5')
 
     return results
 
+
+
+#still in process
 # run the predict from the cloud model.joblib
+storage_client = storage.Client()
+bucket_name='<bucket name>'
+model_bucket='model.joblib'
+
 def predict_cloud():
-    pass
+    bucket = storage_client.get_bucket(bucket_name)
+    #select bucket file
+    blob = bucket.blob(model_bucket)
+    #load that file from local file
+    model = joblib.load(model_bucket)
